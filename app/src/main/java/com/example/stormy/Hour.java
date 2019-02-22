e515a39ef2bd64c0e44a442e72e89d18853093d1
alsamzu <zulfalsam@gmail.com>
2019-02-22 19:59:30 +0300

    Class for hourly weather update

---
+++ app/src/main/java/com/example/stormy/Hour.java
@@ -0,0 +1,50 @@
+package com.example.stormy;
+
+public class Hour {
+    private String mSummary;
+    private long mTime;
+    private double mTemperature;
+    private String mIcon;
+    private String  mTimezone;
+
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+}
